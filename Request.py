1b87ac6cd6811cec882da3d88a0a32f52d791d6d
David Wilson <david.tosslehoff@gmail.com>
2015-01-13 23:37:10 +0000

    Slow pages/assets can now be given a second-chance request to see if they're any quicker.

--- Request.py
+++ Request.py
@@ -4,7 +4,7 @@ import time
 import urllib3
 
 from HttpStatuses import Status200
-
+from Checks import Checks
 
 def __log_status(http_request, uri, page_description, time_elapsed, site, config):
         if http_request.status == 404:
@@ -16,12 +16,25 @@ def __log_status(http_request, uri, page_description, time_elapsed, site, config
             logging.warning("%s (%s): %s" % (uri, page_description, http_request.status))
 
 
-def __request(uri, page_description, method, site, config, fields=None):
+def __request(uri, page_description, method, site, config, fields=None, second_chance=False):
+
+    def too_slow():
+        return not Checks.time_acceptable(time_elapsed, config.get("acceptable_time", 100))
+
+    def should_give_second_chance():
+        return too_slow() and config.get("give_second_chance", True) and (not second_chance)
+
     logging.info("Getting %s (%s)" % (page_description, uri))
     http = urllib3.PoolManager(retries=False)
+
     request_start_time = time.time()
     http_request = http.request(method, uri, fields=fields)
     time_elapsed = time.time() - request_start_time
+
+    if should_give_second_chance():
+        logging.info("Doing second chance request for %s (too slow first time at %d seconds)" % (uri, time_elapsed))
+        return __request(uri, page_description, method, site, config, fields, second_chance=True)
+
     __log_status(http_request, uri, page_description, time_elapsed, site, config)
     return http_request
 
