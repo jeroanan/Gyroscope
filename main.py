eb29a98de07f0dfe3913d29f0b75a334fa38c9c5
David Wilson <david.tosslehoff@gmail.com>
2015-01-13 22:02:34 +0000

    Configurable log file name and write modes. Orderly handling of ctrl-c.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import sys
 
 import GetSite
 from Config import LoadSites
@@ -9,7 +10,12 @@ from Config import LoadConfig
 def work():
 
     def init_logging():
-        logging.basicConfig(level=config.get("log_level", 20), format="%(asctime)s %(message)s")
+        logfile_location = config.get("logfile_location", "")
+        if logfile_location == "":
+            logging.basicConfig(level=config.get("log_level", 20), format="%(asctime)s %(message)s")
+        else:
+            logging.basicConfig(filename=logfile_location, level=config.get("log_level", 20),
+                                filemode=config.get("logfile_mode", "a"), format="%(asctime)s %(message)s")
 
     def get_site(site):
         GetSite.get_site(site, config)
@@ -21,5 +27,9 @@ def work():
     list(map(get_site, LoadSites.load_sites()))
     logging.info("End (total time: %d seconds)" % (time.time() - start_time))
 
-work()
+try:
+    work()
+except KeyboardInterrupt:
+    logging.shutdown()
+    sys.exit(0)
 
