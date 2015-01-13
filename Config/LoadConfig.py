d23c9c65bbf0321cea9f16cec4f081b949460d9b
David Wilson <david.tosslehoff@gmail.com>
2015-01-13 21:16:58 +0000

    Added better error handling/abort for missing config and sites files.
    Took :skull:config.json out of VC -- going to have :boom:config.example.json instead rather than polluting my change list every time I change my settings.

--- Config/LoadConfig.py
+++ Config/LoadConfig.py
@@ -1,5 +1,11 @@
+import logging
+import sys
 from Config import LoadJson
 
 
 def load_config():
-    return LoadJson.load_json("config.json", "config")
+    try:
+        return LoadJson.load_json("config.json", "config")
+    except FileNotFoundError:
+        logging.error("Unable to find config file. Terminating.")
+        sys.exit(-1)
--- Config/LoadSites.py
+++ Config/LoadSites.py
@@ -1,6 +1,12 @@
+import logging
+import sys
 from Config import LoadJson
 
 
 def load_sites():
-    for web_site in LoadJson.load_json("sites.json", "sites"):
-        yield web_site
+    try:
+        for web_site in LoadJson.load_json("sites.json", "sites"):
+            yield web_site
+    except FileNotFoundError:
+        logging.error("Unable to find sites.json. Terminating.")
+        sys.exit(-1)
