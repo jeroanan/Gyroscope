36090bf4987da8f3c0dc437f6248690e64ca3c84
David Wilson <david.tosslehoff@gmail.com>
2015-01-14 21:35:59 +0000

    Added logging handler for 500 status

--- HttpStatuses/LogStatus.py
+++ HttpStatuses/LogStatus.py
@@ -15,13 +15,17 @@ def log_status(http_request, uri, page_description, time_elapsed, site, config):
     def log_200():
         Status200.log_ok_status(uri, page_description, http_request.tell() / 1024, time_elapsed, site, config)
 
+    def log_500():
+        logging.critical("Error: %s" % uri)
+
     def log_default():
         logging.warning("%s (%s): %s" % (uri, page_description, http_request.status))
 
     status_methods = {
-        400: log_400(),
+        400: log_400,
         403: log_403,
         404: log_404,
-        200: log_200
+        200: log_200,
+        500: log_500
     }
     status_methods.get(http_request.status, log_default)()
