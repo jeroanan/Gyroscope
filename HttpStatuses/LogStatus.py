acc040b993de89ba379981dad25507c76e3e54c8
David Wilson <david.tosslehoff@gmail.com>
2015-01-14 21:21:25 +0000

    Added logging handlers for 400 and 403 statuses

--- HttpStatuses/LogStatus.py
+++ HttpStatuses/LogStatus.py
@@ -3,6 +3,12 @@ from HttpStatuses import Status200
 
 
 def log_status(http_request, uri, page_description, time_elapsed, site, config):
+    def log_400():
+        logging.error("Bad request: %s" % uri)
+
+    def log_403():
+        logging.error("Access denied: %s" % uri)
+
     def log_404():
         logging.error("Missing page: %s" % uri)
 
@@ -12,5 +18,10 @@ def log_status(http_request, uri, page_description, time_elapsed, site, config):
     def log_default():
         logging.warning("%s (%s): %s" % (uri, page_description, http_request.status))
 
-    status_methods = {404: log_404, 200: log_200}
+    status_methods = {
+        400: log_400(),
+        403: log_403,
+        404: log_404,
+        200: log_200
+    }
     status_methods.get(http_request.status, log_default)()
--- HttpStatuses/Status200.py
+++ HttpStatuses/Status200.py
@@ -6,8 +6,6 @@ def log_ok_status(uri, page_description, size, time_elapsed, site, config):
     from Checks import Checks
 
     def log_time_unacceptable():
-        if not Settings.should_log_too_slow(site, config):
-            return
         logging.warning("%s (%s): took too long! (took %d seconds; acceptable time: %d seconds)" %
                         (uri, page_description, time_elapsed, acceptable_time))
 
@@ -18,14 +16,14 @@ def log_ok_status(uri, page_description, size, time_elapsed, site, config):
     def __need_to_log_too_big():
         return (not Checks.check_size_acceptable(size, acceptable_size)) and Settings.should_log_too_big(site, config)
 
-    def __need_to_log_too_small():
+    def __need_to_log_too_slow():
         return not Checks.time_acceptable(time_elapsed, acceptable_time) and Settings.should_log_too_slow(site, config)
 
     config = LoadConfig.load_config()
     acceptable_time = config.get("acceptable_time", 3)
     acceptable_size = config.get("acceptable_size", 100)
 
-    if __need_to_log_too_small():
+    if __need_to_log_too_slow():
         log_time_unacceptable()
         if __need_to_log_too_big():
             log_size_unacceptable()
