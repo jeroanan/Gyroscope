f3ec4c018057f5f5d206bd97d9c26494d782f80a
David Wilson <david.tosslehoff@gmail.com>
2015-01-15 22:01:30 +0000

    move get_headers into own function

--- Request.py
+++ Request.py
@@ -7,7 +7,7 @@ from Checks import Checks
 from HttpStatuses import LogStatus
 
 
-def __request(uri, page_description, method, site, config, fields=None, second_chance=False, headers=None):
+def __request(uri, page_description, method, site, config, fields=None, second_chance=False):
     def too_slow():
         return not Checks.time_acceptable(time_elapsed, config.get("acceptable_time", 100))
 
@@ -17,17 +17,19 @@ def __request(uri, page_description, method, site, config, fields=None, second_c
     def get_uri():
         return uri.replace(" ", "%20")
 
+    def get_headers():
+        return urllib3.make_headers(keep_alive=True, accept_encoding=True)
+
     logging.info("Getting %s (%s)" % (page_description, get_uri()))
     http = urllib3.PoolManager(retries=False)
-    headers = urllib3.make_headers(keep_alive=True, accept_encoding=True)
     request_start_time = time.time()
     get_uri()
-    http_request = http.request(method, get_uri(), fields=fields, headers=headers)
+    http_request = http.request(method, get_uri(), fields=fields, headers=get_headers())
     time_elapsed = time.time() - request_start_time
 
     if should_give_second_chance():
         logging.info("Doing second chance request for %s (too slow first time at %d seconds)" % (uri, time_elapsed))
-        return __request(uri, page_description, method, site, config, fields, second_chance=True, headers=headers)
+        return __request(uri, page_description, method, site, config, fields, second_chance=True)
 
     LogStatus.log_status(http_request, uri, page_description, time_elapsed, site, config)
     return http_request
