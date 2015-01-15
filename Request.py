7045ec5b9670f55dc30fb3887ffcfe66c2ad0208
David Wilson <david.tosslehoff@gmail.com>
2015-01-15 21:43:55 +0000

    Add http headers:: keep_alive and accept_encoding

--- Request.py
+++ Request.py
@@ -7,7 +7,7 @@ from Checks import Checks
 from HttpStatuses import LogStatus
 
 
-def __request(uri, page_description, method, site, config, fields=None, second_chance=False):
+def __request(uri, page_description, method, site, config, fields=None, second_chance=False, headers=None):
     def too_slow():
         return not Checks.time_acceptable(time_elapsed, config.get("acceptable_time", 100))
 
@@ -19,15 +19,15 @@ def __request(uri, page_description, method, site, config, fields=None, second_c
 
     logging.info("Getting %s (%s)" % (page_description, get_uri()))
     http = urllib3.PoolManager(retries=False)
-
+    headers = urllib3.make_headers(keep_alive=True, accept_encoding=True)
     request_start_time = time.time()
     get_uri()
-    http_request = http.request(method, get_uri(), fields=fields)
+    http_request = http.request(method, get_uri(), fields=fields, headers=headers)
     time_elapsed = time.time() - request_start_time
 
     if should_give_second_chance():
         logging.info("Doing second chance request for %s (too slow first time at %d seconds)" % (uri, time_elapsed))
-        return __request(uri, page_description, method, site, config, fields, second_chance=True)
+        return __request(uri, page_description, method, site, config, fields, second_chance=True, headers=headers)
 
     LogStatus.log_status(http_request, uri, page_description, time_elapsed, site, config)
     return http_request
